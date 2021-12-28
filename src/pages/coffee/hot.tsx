cc18b08f42bd6d964e8256fd9ed79a91c9f2e454
Kyoo130 <kml9003@kakao.com>
2021-12-28 23:28:27 +0900

    ✨ Hot 페이지 생성

---
+++ src/pages/coffee/hot.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import { HotCard } from "../../components";
+
+const HotPage: NextPage = () => {
+    return (
+        <div>
+            <h1>IcedPage</h1>
+            <hr />
+            <HotCard />
+        </div>
+    )
+}
+
+export default HotPage;
