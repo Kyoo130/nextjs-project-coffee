e6b4a8b230c5735e3e41d8255a06633fa4a04749
Kyoo130 <kml9003@kakao.com>
2021-12-28 23:28:44 +0900

    ✨ Iced 페이지 생성

---
+++ src/pages/coffee/iced.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import { IcedCard } from "../../components";
+
+
+const IcedPage: NextPage = () => {
+    return (
+        <div>
+            <h1>IcedPage</h1>
+            <hr />
+            <IcedCard />
+        </div>
+    )
+}
+
+export default IcedPage;
